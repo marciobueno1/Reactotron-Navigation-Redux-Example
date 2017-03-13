b630fa851e484d0d18219579731ea9a319f93f45
Marcio Bueno <m.bueno@wemersive.com>
2017-03-13 12:36:38 -0300

    using Reactotron store

--- store.js
+++ store.js
@@ -1,9 +1,9 @@
-import { createStore } from 'redux';
-// import Reactotron from './ReactotronConfig';
+// import { createStore } from 'redux';
+import Reactotron from './ReactotronConfig';
 
 import reducer from './reducers';
 
 // const store = createStore(reducer, {});
-const store = createStore(reducer, {}, sorry => sorry);
+const store = Reactotron.createStore(reducer, {}, sorry => sorry);
 
 export default store;
